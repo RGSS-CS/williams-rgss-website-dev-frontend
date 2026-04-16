586fa5d1cb4187a347adf0f00565861e40e39091
jerrf010 <huntingaiden513@gmail.com>
2026-04-16 17:46:49 -0400

    Remove unused Clubs layout component and clean up page.tsx; add global CSS for styling

--- frontend/app/Public/Clubs/page.tsx
+++ frontend/app/Public/Clubs/page.tsx
@@ -1,6 +1,6 @@
 import link from "next/link";
 import Image from "next/image";
-import { Club } from "@/types/club";
+// import { Club } from "@/types/club";
 
 type Club = {
     id: number;
@@ -8,17 +8,17 @@ type Club = {
     description: string;
 }
 
-async function getClubs(): Promise<Club[]> {
-    const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/clubs', {
-        cache: "no-store",
-    });
+//async function getDjangoAPI(): Promise<Club[]> {
+//    const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/clubs', {
+//        cache: "no-store",
+//    });
 
-if (!res.ok) throw new Error("Failed to fetch clubs");
-return res.json();
-}
+//    if (!res.ok) throw new Error("Failed to fetch clubs");
+//    return res.json();
+//}
 
 export default async function ClubsPage() {
-    const clubs = await getClubs();
+    //const clubs = await getDjangoAPI();
     return (
         <main>
             <h1 className="text-3xl font-bold mb-6">Clubs</h1>
