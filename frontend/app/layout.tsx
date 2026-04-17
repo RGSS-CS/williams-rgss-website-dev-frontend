ed8728e75c5f16acd3eae9e62c6b31a07e8b5c95
jerrf010 <huntingaiden513@gmail.com>
2026-04-17 17:05:12 -0400

    Confirm Final Design for DEFAULT layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,8 @@
 // These styles apply to every route in the application
 import './globals.css';
 import type { Metadata } from 'next';
- 
+import Image from 'next/image';
+
 export const metadata: Metadata = {
   title: 'Dr. GW Williams Secondary School STUCO',
   description: 'STUCO Cloud Portal for Dr. GW Williams Secondary School',
@@ -16,26 +17,28 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {/* Font Awesome CDN Link */}
-      <link rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-        crossOrigin='anonymous'
-        referrerPolicy='no-referrer'
-      />
-      {/* Google Fonts CDN Link */}
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+          crossOrigin='anonymous'
+          referrerPolicy='no-referrer'
+        />
+        {/* Google Fonts CDN Link */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
       </head>
       <body>
         <div className="navbar">
           <div className="header-container">
             <div className="title-container">
-              <div className="title-title">
-                <h1>Dr. GW Williams Secondary School</h1>
-                <span>Student Council</span>
-              </div>
               <div className="logo">
-                </div>
+                <Image src="/images/logo/logo.png" alt="School Logo" width={80} height={60} />
+              </div>
+              <div className="title-title">
+                <span id="school-title"><h1>Dr. GW Williams S.S.</h1></span>
+                <span id="school-subtitle">Student Council</span>
+              </div>
+
             </div>
 
             <div className="nav-links">
@@ -51,7 +54,7 @@ export default function RootLayout({
           <div className="footer-container">
             <p>&copy; {currentYear} Dr. GW Williams Secondary School Student Council. All rights reserved.</p>
             <div className="footer-socials">
-              <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+              <a href="https://www.instagram.com/drgwwilliams/" target="_blank" rel="noopener noreferrer">
                 <i className="fab fa-instagram"></i>
               </a>
               <a href="https://drgwwilliams-ss.yrdsb.ca/" target="_blank" rel="noopener noreferrer">
