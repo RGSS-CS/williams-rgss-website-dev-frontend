0181f25bcda1dc5715363e5865ebd5a5b13acdf1
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-04-17 11:59:20 -0400

    feat: create homepage redirect to public and add ClubsPage component

---
+++ frontend/app/public/page.tsx
@@ -0,0 +1,30 @@
+
+import link from "next/Link";
+import Image from "next/image";
+import styles from './clubs.module.css';
+// import { Club } from "@/types/club";
+
+type Club = {
+    id: number;
+    name: string;
+    description: string;
+}
+
+//async function getDjangoAPI(): Promise<Club[]> {
+//    const res = await fetch('${process.env.NEXT_PUBLIC_API_URL}/api/clubs', {
+//        cache: "no-store",
+//    });
+
+//    if (!res.ok) throw new Error("Failed to fetch clubs");
+//    return res.json();
+//}
+
+export default async function ClubsPage() {
+    //const clubs = await getDjangoAPI();
+    return (
+        <main>
+            <h1>Homepage</h1>
+            <div className="navbar"></div>
+        </main>
+    );
+}
