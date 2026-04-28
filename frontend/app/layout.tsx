261203439c6c688c4e0565d6272d4aa262deff62
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-04-28 09:16:54 -0400

    refactor: update layout structure and remove unused metadata; streamline head elements

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,13 +1,33 @@
 import './global.css'
- 
+import type { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+    title: 'Dr. GW Williams STUCO',
+    description: 'STUCO Cloud Portal for Dr. GW Williams Secondary School',
+};
+
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head>
+                {/* Font Awesome CDN Link */}
+                <link rel="stylesheet"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+                    crossOrigin='anonymous'
+                    referrerPolicy='no-referrer'
+                />
+                {/* Google Fonts CDN Link */}
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+                <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
+            </head>
+            {children}
+        </html>
+    )
 }
--- frontend/app/public/layout.tsx
+++ frontend/app/public/layout.tsx
@@ -1,11 +1,6 @@
 import './styles.css';
-import type { Metadata } from 'next';
 import Navbar from '@/app/public/ui/navbar';
 
-export const metadata: Metadata = {
-  title: 'Dr. GW Williams STUCO',
-  description: 'STUCO Cloud Portal for Dr. GW Williams Secondary School',
-};
 export default function RootLayout({
   children,
 }: {
@@ -14,18 +9,7 @@ export default function RootLayout({
   const currentYear = new Date().getFullYear();
   return (
     <html lang="en">
-      <head>
-        {/* Font Awesome CDN Link */}
-        <link rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
-          crossOrigin='anonymous'
-          referrerPolicy='no-referrer'
-        />
-        {/* Google Fonts CDN Link */}
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
-      </head>
+
       <body>
         <Navbar />
         {children}
