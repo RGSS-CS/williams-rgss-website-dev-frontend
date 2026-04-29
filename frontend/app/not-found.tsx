c00f61886a2fba42f0d6355d662e5fa9f97697a9
jerrf010 <huntingaiden513@gmail.com>
2026-04-29 19:28:30 -0400

    Fix indentation and formatting in NotFound component for improved readability

--- frontend/app/not-found.tsx
+++ frontend/app/not-found.tsx
@@ -4,27 +4,27 @@ import styles from '@/app/_modules/not-found.module.css'
 
 export default function NotFound() {
     return (
-        <div className={styles.content}>
-            <div className={styles.gif_stage}>
-                <div className={styles.overlay_404}>
-                    <div className={styles.num_404}>4<span>0</span>4</div>
+            <div className={styles.content}>
+                <div className={styles.gif_stage}>
+                    <div className={styles.overlay_404}>
+                        <div className={styles.num_404}>4<span>0</span>4</div>
+                    </div>
+                    <div className={styles.gif_frame}>
+                        <img src="/images/gifs/meowl.gif" alt="meow"></img>
+                    </div>
                 </div>
-                <div className={styles.gif_frame}>
-                    <img src="/images/gifs/meowl.gif" alt="meow"></img>
+
+                <div className={styles.copy}>
+                    <h1>😨</h1>
+                    <p>The page you're looking for doesn't exist or may have been moved. Head back to safety.</p>
+                </div>
+
+                <div className={styles.actions}>
+                    <a href="/" className={styles.btn_home}>
+                        <i className="fa-solid fa-house"></i>
+                        Go to Home
+                    </a>
                 </div>
             </div>
-
-            <div className={styles.copy}>
-                <h1>😨</h1>
-                <p>The page you're looking for doesn't exist or may have been moved. Head back to safety.</p>
-            </div>
-
-            <div className={styles.actions}>
-                <a href="/" className={styles.btn_home}>
-                    <i className="fa-solid fa-house"></i>
-                    Go to Home
-                </a>
-            </div>
-        </div>
     )
 }
