55a7f84350a798c319e22f76a4770c7ad6010afd
jerrf010 <huntingaiden513@gmail.com>
2026-04-30 18:18:32 -0400

    Refactor Navbar and Footer components; update import paths and enhance styling for layout

--- frontend/app/(public)/layout.tsx
+++ frontend/app/(public)/layout.tsx
@@ -1,8 +1,8 @@
 // These styles apply to every route in the application
 import './styles.css';
 import type { Metadata } from 'next';
-import Navbar from '@/app/_ui/navbar';
-import Footer from '@/app/_ui/footer';
+import Navbar from '@/app/(public)/_ui/navbar';
+import Footer from '@/app/(public)/_ui/footer';
 
 export const metadata: Metadata = {
   title: 'Dr. GW Williams STUCO',
--- frontend/app/private/authentication/layout.tsx
+++ frontend/app/private/authentication/layout.tsx
@@ -1,5 +1,6 @@
-import Navbar from "@/app/_ui/navbar";
-import Footer from "@/app/_ui/footer";
+import Image from "next/image";
+import "@/app/(public)/styles.css";
+import Footer from "@/app/(public)/_ui/footer";
 
 export default function RootLayout({
     children,
@@ -8,7 +9,19 @@ export default function RootLayout({
 }) {
     return (
         <main>
-            <Navbar />
+            <nav className="navbar">
+                <div className="header-container">
+                    <a className="title-container" href="/public">
+                        <div className="logo">
+                            <Image src="/images/logo/logo.png" alt="School Logo" width={80} height={60} />
+                        </div>
+                        <div>
+                            <span className="school-title"><h1>Dr. GW Williams S.S.</h1></span>
+                            <span className="school-subtitle">Student Council</span>
+                        </div>
+                    </a>
+                </div>
+            </nav>
             {children}
             <Footer />
         </main>
