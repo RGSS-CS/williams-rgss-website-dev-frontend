750f9f29970294eb2ee7f163ed70e762661e56f0
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-05-07 13:53:20 -0400

    Fix styling for titles on gallery page and adjust layout structure (#85)

--- frontend/app/(public)/gallery/page.tsx
+++ frontend/app/(public)/gallery/page.tsx
@@ -10,17 +10,19 @@ export default function GalleryPage() {
         <main>
             <div className="hero">
                 <div className="hero_shape"></div>
-                <div className="hero_left">
-                    <div className="hero_title">
-                        <h1>Gallery</h1>
-                    </div>
-                    <div className="hero_subtitle">
-                        <h5>Where photos tell our story.</h5>
-                        <h5>Events, clubs, competitions, and everyday Wildcat moments.</h5>
-                    </div>
-                    <div className="search_container">
-                        <i className="fas fa-search"></i>
-                        <input className="search_input" id="gallery_search" type="text" placeholder="Search by club name, event, people..."></input>
+                <div className="hero_inner">
+                    <div className="hero_left">
+                        <div className="hero_title">
+                            <h1>Gallery</h1>
+                        </div>
+                        <div className="hero_subtitle">
+                            <h5>Where photos tell our story.</h5>
+                            <h5>Events, clubs, competitions, and everyday Wildcat moments.</h5>
+                        </div>
+                        <div className="search_container">
+                            <i className="fas fa-search"></i>
+                            <input className="search_input" id="gallery_search" type="text" placeholder="Search by club name, event, people..."></input>
+                        </div>
                     </div>
                 </div>
             </div>
