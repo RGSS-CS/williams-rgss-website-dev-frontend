dfa4e583da1f6718811c482b0f20a5c39f743bef
jerrf010 <huntingaiden513@gmail.com>
2026-05-24 22:10:35 -0400

    FIX link to login at register page

--- frontend/app/private/authentication/register/page.tsx
+++ frontend/app/private/authentication/register/page.tsx
@@ -6,7 +6,7 @@ export default function SignupForm() {
       <div className={`${styles.login_card} ${styles.register_card}`}>
         <div className={styles.card_header}>
           <h2>Please notify STUCO</h2>
-          <p>Please register your account with STUCO first, then return to <a href="/private/authentication/login">login</a></p>
+          <p>Please register your account with STUCO first, then return to <a href="/private/authentication">login</a></p>
         </div>
       </div>
     </div>
