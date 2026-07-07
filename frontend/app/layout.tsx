68775bee264f4caa4b921822cd076bff9fddf0bd
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-07-07 10:39:33 -0400

    remove unnecessary components that possibiliy causes an error

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,20 +1,10 @@
 import { Montserrat, Jost, Space_Grotesk, Figtree, IBM_Plex_Sans } from "next/font/google";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-
-/* import all the icons in Free Solid, Free Regular, and Brands styles */
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { far } from '@fortawesome/free-regular-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-library.add(fas, far, fab)
-config.autoAddCss = false
-
-
 import "@/app/global.css";
 
+/* import all the icons in Free Solid, Free Regular, and Brands styles */
+config.autoAddCss = false
 
 const montserrat = Montserrat({
     subsets: ["latin"],
