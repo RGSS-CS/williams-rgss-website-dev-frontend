c4c9b2f734a5618107840f8f657b1283e9b2ecd2
jerrf010 <huntingaiden513@gmail.com>
2026-07-24 12:04:09 -0400

    add metadata

--- frontend/app/private/(authenticated)/dashboard/page.tsx
+++ frontend/app/private/(authenticated)/dashboard/page.tsx
@@ -1,4 +1,11 @@
 import { getManagementSettings } from "@/app/_lib/management";
+import { getSiteMetadata } from "@/app/_utils/metadata";
+import { Metadata } from "next";
+import styles from "dashboard.module.css";
+
+export async function generateMetadata(): Promise<Metadata> {
+    return getSiteMetadata("Exec Dashboard");
+}
 
 export default async function ExecDashboard() {
     const management = await getManagementSettings();
