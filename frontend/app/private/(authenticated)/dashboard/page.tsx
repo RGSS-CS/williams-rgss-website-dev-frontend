a9c4cd18a20679afa6c19d04cb8e0aac4436293c
jerrf010 <huntingaiden513@gmail.com>
2026-07-23 23:20:47 -0400

    do some stuff

--- frontend/app/private/(authenticated)/dashboard/page.tsx
+++ frontend/app/private/(authenticated)/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import ExecFooter from "./_components/Footer";
+
 export default function ExecDashboard() {
     return (
         <main>
-            
+            <ExecFooter />
         </main>
     );
 }
