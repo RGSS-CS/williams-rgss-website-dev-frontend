caf82ed2d83d080a20cdb9434439c2596f4e26ed
jerrf010 <huntingaiden513@gmail.com>
2026-08-04 18:02:46 -0400

    update clubs page to page settings

--- frontend/app/(public)/clubs/page.tsx
+++ frontend/app/(public)/clubs/page.tsx
@@ -3,6 +3,7 @@ import { getManagementSettings } from "@/app/_lib/site-management";
 import { getSiteMetadata } from "@/app/_utils/metadata";
 import ClubsDirectory from "./_ClubsDirectory";
 import { Metadata } from 'next';
+import { getPageManagementSettings } from "@/app/_lib/page-management";
 
 export async function generateMetadata(): Promise<Metadata> {
   return getSiteMetadata("Clubs");
@@ -11,6 +12,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function ClubsPage() {
     const clubs = await getClubs();
     const management = await getManagementSettings();
+    const pageManagement = await getPageManagementSettings("CL");
 
-    return <ClubsDirectory clubs={clubs} management={management} />;
+    return <ClubsDirectory clubs={clubs} management={management} pageManagement={pageManagement} />;
 }
