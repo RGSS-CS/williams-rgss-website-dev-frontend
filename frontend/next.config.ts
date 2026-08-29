1b5cc926b40a07aebf9db34ae08bef26ecc7c67c
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-08-29 16:17:18 -0400

    fix images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,13 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    globalNotFound: true,
-  },
-  devIndicators: false,
-  output: 'standalone',
-  cacheComponents: true,
-  agentRules: false,
-};
-
-export default nextConfig;
+    experimental: {
+        globalNotFound: true,
+    },
+    devIndicators: false,
+    output: 'standalone',
+    cacheComponents: true,
+    agentRules: false,
+    images: {
+        remotePatterns: [new URL('http://localhost:8000/media/**'), new URL("http://dev.rgsscs.org/media/**")], //Add env for backend 
+    }
+}
