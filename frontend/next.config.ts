0e7110d3546164f3260196f195e56a41a5b639c1
AidenSo <155278333+jerrf010@users.noreply.github.com>
2026-08-29 10:10:15 -0400

    update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
     cacheComponents: true,
     agentRules: false,
     images: {
-        remotePatterns: [new URL('http://localhost:8000/media/**')], //Add env for backend 
-        dangerouslyAllowLocalIP: true //TODO: REMOVE LATER
+        remotePatterns: [new URL('http://localhost:8000/media/**'), new URL('http://backend:8000/media/**')], //Add env for backend 
+        //dangerouslyAllowLocalIP: true //TODO: REMOVE LATER
     }
 };
 
